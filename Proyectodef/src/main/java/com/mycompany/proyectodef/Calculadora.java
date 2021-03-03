bd5f80d3096d4634c46ea97cf76145582f475802
Cristina <cristina.marce.acosta@gmail.com>
2021-03-03 16:44:21 +0000

    Paso 3 y 4

--- Proyectodef/src/main/java/com/mycompany/proyectodef/Calculadora.java
+++ Proyectodef/src/main/java/com/mycompany/proyectodef/Calculadora.java
@@ -9,4 +9,46 @@ public class Calculadora {
     private String operador1;
     private String operador2;
     private String operacion;
+
+    /**
+     * @return the operador1
+     */
+    public String getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(String operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the operador2
+     */
+    public String getOperador2() {
+        return operador2;
+    }
+
+    /**
+     * @param operador2 the operador2 to set
+     */
+    public void setOperador2(String operador2) {
+        this.operador2 = operador2;
+    }
+
+    /**
+     * @return the operacion
+     */
+    public String getOperacion() {
+        return operacion;
+    }
+
+    /**
+     * @param operacion the operacion to set
+     */
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
 }
