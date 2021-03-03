3b2c13ee3e4ce21eef61c93ea88059e4c484900b
Cristina <cristina.marce.acosta@gmail.com>
2021-03-03 16:41:32 +0000

    Paso 1 y 2

---
+++ Proyectodef/src/main/java/com/mycompany/proyectodef/Calculadora.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectodef;
+
+public class Calculadora {
+    private String operador1;
+    private String operador2;
+    private String operacion;
+}
